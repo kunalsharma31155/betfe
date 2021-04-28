e33d969ddd64f4cffe614e49ed05bd3a2024d659
emicon-web <emiconweb@emicon.co.in>
2021-04-28 18:17:14 +0530

    fcd

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname+'./dist/client'));
+app.use(express.static(__dirname+'/dist/client'));
 
 app.get('/*', (req, res) =>
-    res.sendFile(path.join(__dirname+'./dist/client/index.html')),
+    res.sendFile(path.join(__dirname+'/dist/client/index.html')),
 );
 var PORT = process.env.PORT || 5000;
 
